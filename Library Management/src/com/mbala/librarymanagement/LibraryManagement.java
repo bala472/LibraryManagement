b9b0e32c60445a068b9d4f001cf49cd3be2042c7
bala472 <160371380+bala472@users.noreply.github.com>
2024-03-15 18:33:57 +0530

    Delete Library Management directory

--- Library Management/src/com/mbala/librarymanagement/LibraryManagement.java
+++
@@ -1,7 +0,0 @@
-package com.mbala.librarymanagement;
-import com.mbala.librarymanagement.login.LoginView;
-public class LibraryManagement {
-    public static void main(String[] args) {
-
-        }
-    }
