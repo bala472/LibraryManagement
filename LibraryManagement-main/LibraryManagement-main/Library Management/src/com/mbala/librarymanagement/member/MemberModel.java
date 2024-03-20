3e3631c01b15aa994eacf8e27da1282e3a859ccd
bala472 <160371380+bala472@users.noreply.github.com>
2024-03-20 18:58:49 +0530

    Delete LibraryManagement-main/LibraryManagement-main/Library Management directory

--- LibraryManagement-main/LibraryManagement-main/Library Management/src/com/mbala/librarymanagement/member/MemberModel.java
+++
@@ -1,8 +0,0 @@
-package com.mbala.librarymanagement.member;
-
-public class MemberModel {
-    private MemberView memberView;
-    public MemberModel(MemberView memberView){
-        this.memberView=memberView;
-    }
-}
